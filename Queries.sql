fb7ff521154bca1d4ecd2f7bbd8e855955d69984
UdayasGunasekaran <udayas0907@gmail.com>
2025-01-29 09:52:47 +0530

    Add files via upload
    I have added my data analysis project's source file, queries and dashboard in pbix, pdf formats

---
+++ Queries.sql
@@ -0,0 +1,8 @@
+/*SELECT * FROM project.phphone_usage_india;*/
+describe project.phphone_usage_india;
+/*select * from project.phphone_usage_india where Age >= 18;*/
+/*select * from project.phphone_usage_india where OS = "iOS";*/
+/*select Sum(Age)/count(Age) as Average_Age from project.phphone_usage_india;*/
+
+
+
